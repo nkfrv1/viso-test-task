8cb8d06eecb8c2d10bb5dd1187102d93af25136b
nkfrv <i.nikiforov1111@gmail.com>
2024-11-27 19:11:52 +0200

    connect typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-    imports: [ConfigModule.forRoot({ isGlobal: true, expandVariables: true })],
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true, expandVariables: true }),
+        TypeOrmModule.forRootAsync({
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => ({
+                type: 'postgres',
+                url: configService.get('DB_CONNECTION_URI'),
+                autoLoadEntities: true,
+                logging: true,
+            }),
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
