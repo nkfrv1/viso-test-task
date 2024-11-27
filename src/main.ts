18fdea8f7dd058971d4b3bffe7c7ef6e7279eada
nkfrv <i.nikiforov1111@gmail.com>
2024-11-27 16:21:24 +0200

    enable global validation

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            forbidNonWhitelisted: true,
+            transform: true,
+            transformOptions: { enableImplicitConversion: true },
+        }),
+    );
+
     const configService = app.get(ConfigService);
     app.enableCors();
+
     await app.listen(+configService.get('PORT') || 3000);
 }
 
